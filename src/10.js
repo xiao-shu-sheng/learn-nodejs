06afeb3757e93761a853ee89c0bcf94bed07d754
2418356743@qq.com <2418356743@qq.com>
2024-07-29 07:48:57 +0800

    fs

--- src/10.js
+++ src/10.js
@@ -7,9 +7,9 @@ import fs2 from 'node:fs/promises'
 // 删除 rm
 // 重命名 renameSync
 // 监听文件变化watch
-// 源码libuv
-// 事件循环 setTimediate
-
+// 写入文件、追加写入
+// 创建可写流
+// 软连接 硬链接
 // fs.readFile('./test.js', {
 //     encoding: 'utf-8',
 //     flag: 'r'
@@ -60,3 +60,39 @@ import fs2 from 'node:fs/promises'
 // fs.watch('./index.js',(event,filename) => {
 //     console.log(event, filename)
 // })
+
+
+fs.writeFileSync("./test.js", "\nconsole.log('mother fuck')", {
+    flag: 'a'
+})
+
+fs.appendFileSync("./test.js", "\nconsole.log('阿西吧')", {
+    flag: 'a'
+})
+
+
+// 创建可写流， 处理大量数据
+// let writeStream = fs.createWriteStream('./test.txt')
+// const ary = [
+//     '二八佳人体似酥',
+//     '腰间仗剑斩愚夫',
+//     '虽然不见人头落',
+//     '暗里教君骨髓枯'
+// ]
+// ary.forEach(i => {
+//     writeStream.write(i + '\n')
+// })
+// writeStream.end()
+// writeStream.on('finish',() => {
+//     console.log('我写完了')
+// })
+
+
+// 硬链接
+//          原始地址       硬链接之后的地址  共享/备份
+// fs.linkSync('./test.txt', './test2.txt')
+
+
+// 软连接 类似windows的快捷方式 需要管理员权限
+fs.symlinkSync('./test.txt', './test2.txt')
+
--- src/test.js
+++ src/test.js
@@ -4,3 +4,11 @@ process.on('message', (msg) => {
     console.log('get message', msg)
 })
 process.send('call me father')
+
+
+console.log('mother fuck')
+console.log('阿西吧')
+console.log('mother fuck')
+console.log('阿西吧')
+console.log('mother fuck')
+console.log('阿西吧')
