6eafa5aed97f0f3c4d47f2e2fad60cca6932f039
wszq314618 <2418356743@qq.com>
2024-08-28 23:06:42 +0800

    修改

--- index.js
+++ index.js
@@ -17,5 +17,17 @@
 // nodejs 自己实现了一个console api 与浏览器的console api 不一样 能够使打印的内容可以在终端上看到
 console.log(process.env.NODE_ENV == 'dev' ? '叫爸爸' :  'call me father')
 
+/** 
+ * 浏览器一帧60fps左右的， 1000/60 = 16.6ms
+ * 1、处理用户事件、如click,input change
+ * 2、执行定时器任务
+ * 3、执行动画requestAnimationFrame
+ * 4、执行dom回流与重绘
+ * 5、计算更新图层的绘制指令
+ * 6、绘制指令合并主线程
+ */ 
+
+
+
 
 
