09c8cdf07bbfbec7408c6d2133c5d2ab2e7efadf
ZQ\24183 <2418356743@qq.com>
2024-09-09 09:09:08 +0800

    修改

--- src/js/01/02.js
+++ src/js/01/02.js
@@ -100,7 +100,7 @@ class Van extends Vehicle {}
 
 new Van() */
 
-class SuperArray extends Array {
+/* class SuperArray extends Array {
   shuffle() {
     for (let i = this.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -111,4 +111,4 @@ class SuperArray extends Array {
 }
 let a = new SuperArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 a.shuffle()
-console.log(a)
+console.log(a) */
